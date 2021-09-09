a606d79c2542b99d35df36342b91234ad909b75f
komeetta1 <perttu.tahtinen@gmail.com>
2021-09-09 11:23:12 +0300

    hello world

---
+++ index.js
@@ -0,0 +1,12 @@
+//https://github.com/komeetta1/BackendWithNode
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// serve the public directory as a static content here
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
